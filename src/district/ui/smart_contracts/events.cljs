0409cc123e1eab85a7a7bcaed9c6eed89566359e
Juan Monetta <jpmonettas@gmail.com>
2018-12-10 10:29:03 -0300

    New semantics for different contract files formats

--- src/district/ui/smart_contracts/events.cljs
+++ src/district/ui/smart_contracts/events.cljs
@@ -46,19 +46,24 @@
        (get-version-param version contracts-version)))
 
 
-(defn- contracts-to-load [{:keys [:contracts :load-bin? :abi-file-type] :as opts}]
+(defn- files-to-load [{:keys [:contracts :load-bin? :format]
+                       :or {format :solc-abi-bin}
+                       :as opts}]
   (reduce
     (fn [acc [key {:keys [:abi :bin] :as contract}]]
       (cond-> acc
-        (not abi) (conj {:file-path (get-file-path abi-file-type contract opts)
+        (not abi) (conj {:file-path (get-file-path ({:solc-abi-bin :abi
+                                                     :truffle-json :json} format)
+                                                   contract opts)
                          :contract-key key
                          :file-type :abi
-                         :abi-file-type abi-file-type})
+                         :format format})
 
         (and load-bin?
-             (not bin)) (conj {:file-path (get-file-path :bin contract opts)
-                               :contract-key key
-                               :file-type :bin})))
+             (not bin)
+             (= format :solc-abi-bin)) (conj {:file-path (get-file-path :bin contract opts)
+                                              :contract-key key
+                                              :file-type :bin})))
     []
     contracts))
 
@@ -66,10 +71,9 @@
 (reg-event-fx
   ::load-contracts
   [interceptors (validate-first-arg :district.ui.smart-contracts/opts)]
-  (fn [{:keys [:db]} [{:keys [:request-timeout :contracts :abi-file-type] :as opts
-                       :or {request-timeout 10000
-                            abi-file-type :abi}}]]
-    (let [to-load (contracts-to-load opts)
+  (fn [{:keys [:db]} [{:keys [:request-timeout :contracts] :as opts
+                       :or {request-timeout 10000}}]]
+    (let [to-load (files-to-load opts)
           *load-batch* (atom (zipmap to-load (repeat false)))]
       {:db (queries/merge-contracts db contracts)
        :http-xhrio
@@ -87,13 +91,15 @@
 (reg-event-fx
   ::contract-loaded
   interceptors
-  (fn [{:keys [:db]} [{:keys [:contract-key :file-type :abi-file-type] :as contract} success? *load-batch* response]]
+  (fn [{:keys [:db]} [{:keys [:contract-key :file-type :format] :as contract} success? *load-batch* response]]
     (swap! *load-batch* assoc contract true)
     (let [new-db (if success?
                    (condp = file-type
-                     :abi (queries/assoc-contract-abi db contract-key (case abi-file-type
-                                                                        :abi response
-                                                                        :json (get response "abi")))
+                     :abi (case format
+                            :solc-abi-bin (queries/assoc-contract-abi db contract-key response)
+                            :truffle-json (-> db
+                                              (queries/assoc-contract-abi contract-key (get response "abi"))
+                                              (queries/assoc-contract-bin contract-key (get response "bytecode"))))
                      :bin (queries/assoc-contract-bin db contract-key response))
                    db)]
       (merge
