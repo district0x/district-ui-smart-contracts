2668f79eb777e6d1945d2ef25ee6c962b927deb8
Juan Monetta <jpmonettas@gmail.com>
2018-12-21 08:03:18 -0300

    Revert "Changing smart-contract abi config from env to closure-defines"
    This reverts commit b405d39679a89a76294bc2c7c2055d34ff043a9e.

--- src/district/ui/smart_contracts/utils.clj
+++ src/district/ui/smart_contracts/utils.clj
@@ -1,11 +1,6 @@
 (ns district.ui.smart-contracts.utils
   (:require [clojure.string :as str]
-            [clojure.data.json :as json]
-            [cljs.core]))
-
-(cljs.core/goog-define smart-contracts "")
-(cljs.core/goog-define smart-contracts-build-path "")
-(cljs.core/goog-define smart-contracts-skip "")
+           [clojure.data.json :as json]))
 
 (defn get-abi-from-truffle-art [json-path]
   (-> (slurp json-path)
@@ -13,14 +8,14 @@
       :abi))
 
 (defmacro slurp-env-contracts []
-  (let [smart-contracts-file smart-contracts
-        smart-contracts-build-path smart-contracts-build-path
-        skip-contracts (when (not-empty smart-contracts-skip)
-                         (->> (str/split smart-contracts-skip #",")
+  (let [smart-contracts-file (System/getenv "SMART_CONTRACTS")
+        smart-contracts-build-path (System/getenv "SMART_CONTRACTS_BUILD_PATH")
+        skip-contracts (when-let [scs (System/getenv "SMART_CONTRACTS_SKIP")]
+                         (->> (str/split scs #",")
                               (mapv keyword)))]
     (binding [*out* *err*]
       (println "SKIPPING CONTRACTS " skip-contracts)
-      (if (and (not-empty smart-contracts-file) (not-empty smart-contracts-build-path))
+      (if (and smart-contracts-file smart-contracts-build-path)
         (let [[_ _ smart-contracts-map] (->> (slurp smart-contracts-file)
                                              (format "[%s]")
                                              read-string
